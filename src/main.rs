d432ada9001a8c0b3b80d37d5662ffd2f2121ad1
Ethanol <119572743+Swoiksdmop@users.noreply.github.com>
2023-05-14 19:28:45 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut x = String::new();
+
+    println!("Say something");
+
+    io::stdin().read_line(&mut x).expect("Failed to read line");
+
+    println!("You said: {x}");
+}
